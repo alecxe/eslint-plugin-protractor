84b3da235eeb05b87afedf1314c1fa27cfa3b4a2
Alexander Afanasyev <afanasieffav@gmail.com>
2016-02-04 00:30:08 -0500

    fix(rules): fixed the no-browser-sleep rule initialization

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 module.exports = {
   rules: {
     'missing-perform': require('./lib/rules/missing-perform'),
-    'missing-wait-message': require('./lib/rules/missing-wait-message')
+    'missing-wait-message': require('./lib/rules/missing-wait-message'),
+    'no-browser-sleep': require('./lib/rules/no-browser-sleep')
   },
   rulesConfig: {
     'missing-perform': 2,
